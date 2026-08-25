8358aad5dd4e125cd03db42ff591135d22b6e166
Guillermo López Rodríguez <guillermin97@gmail.com>
2026-08-25 23:16:21 +0200

    fix for classic

--- DisableBlizzardCastBar.lua
+++ DisableBlizzardCastBar.lua
@@ -2,12 +2,13 @@ local disableCastBar = CreateFrame("Frame")
 disableCastBar:RegisterEvent("PLAYER_ENTERING_WORLD")
 disableCastBar:SetScript("OnEvent", function()
     -- print(select(4, GetBuildInfo()))
-    if WOW_PROJECT_ID == WOW_PROJECT_MAINLINE then
-        PlayerCastingBarFrame:UnregisterAllEvents()
-    elseif WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC then
-        PlayerCastingBarFrame:UnregisterAllEvents()
-    else
-        CastingBarFrame:UnregisterAllEvents()
-    end
+    -- if WOW_PROJECT_ID == WOW_PROJECT_MAINLINE then
+    --     PlayerCastingBarFrame:UnregisterAllEvents()
+    -- elseif WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC then
+    --     PlayerCastingBarFrame:UnregisterAllEvents()
+    -- else
+    --     PlayerCastingBarFrame:UnregisterAllEvents()
+    -- end
+    PlayerCastingBarFrame:UnregisterAllEvents()
     disableCastBar:UnregisterEvent("PLAYER_ENTERING_WORLD")
 end)
